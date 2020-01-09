43e9970c127986df659401356fc8fde8bd7fafa6
Karen V <karenvo@usc.edu>
2020-01-09 22:42:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+
+  };
+
+  static santitize(str) {
+    // removes non-alphanumeric characters except for dash, single quote, and space
+  };
+
+  static titleize(str) {
+    // capitalizes all words in a stentence xcept 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'
+      // always capitalizes the first word
+  };
 }
