3775cb076782281937e3ea27aace196ac4a2718f
Karen V <karenvo@usc.edu>
2020-01-09 23:03:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ class Formatter {
     // capitalizes all words in a stentence xcept 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'
       // always capitalizes the first word
       let array = str.split(' ')
-      let exception = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      //let exception = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
       let newArray = []
       let upcasearray = array.map(word => {
         if ( word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but' && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from') {
