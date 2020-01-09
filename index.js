c5ad7230526dad7a8a020e30ecb7594f109b7c8a
Karen V <karenvo@usc.edu>
2020-01-09 23:02:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,5 +14,16 @@ class Formatter {
   static titleize(str) {
     // capitalizes all words in a stentence xcept 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', and 'from'
       // always capitalizes the first word
-  };
+      let array = str.split(' ')
+      let exception = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+      let newArray = []
+      let upcasearray = array.map(word => {
+        if ( word !== 'the' && word !== 'a' && word !== 'an' && word !== 'but' && word !== 'of' && word !== 'and' && word !== 'for' && word !== 'at' && word !== 'by' && word !== 'from') {
+          newArray.push(word.charAt(0).toUpperCase() + word.slice(1))
+        } else {
+          newArray.push(word)
+        }
+      }
+      return newArray.join(' ').charAt(0).toUpperCase() + newArray.join(' ').slice(1)
+    };
 }
